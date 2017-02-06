a3a063c92f7cfc4141971d43709cebe40248b77b
jeremy <kepung@gmail.com>
2017-02-06 15:17:15 +1300

    bind feed features to job applied.

--- src/pages/job/JobAppliedPage.ts
+++ src/pages/job/JobAppliedPage.ts
@@ -1,22 +1,23 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 import { JobFeedService } from "../../providers/job-feed-service"
 
 @Component({
   selector: 'page-page1',
-  templateUrl: 'jobAppliedPage.html'
+  templateUrl: 'jobAppliedPage.html', 
+  providers: [JobFeedService]
 })
 
 export class JobAppliedPage {
+  data: any;
 
-  data : any;
+  constructor(public navCtrl: NavController, private feedService: JobFeedService, private loader: LoadingController) {
 
-  constructor(public navCtrl: NavController, private feedService: JobFeedService) {
-     feedService.getFeed().then(data => {
+    let loadingUI = this.loader.create({ content: 'Please wait ....' });
+    loadingUI.present();
+    this.feedService.getFeed().then(data => {
       this.data = data;
+      loadingUI.dismiss();
     });
-  }
-
-  getItems(ev) {
   }
 }
